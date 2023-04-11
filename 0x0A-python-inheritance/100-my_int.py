54a33092c994215a3e38b707b2efa38c10a5c0ca
David Muindi <muindidiego@gmail.com>
2023-04-11 07:05:30 -0700

    Task 12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+
 class MyInt(int):
+    """Class MyInt that inherits from int.
+
+    MyInt is a rebel. MyInt has == and != operators inverted.
+    """
+
     def __eq__(self, other):
+        """Override the == operator to invert its behavior."""
         return super().__ne__(other)
 
     def __ne__(self, other):
+        """Override the != operator to invert its behavior."""
         return super().__eq__(other)
+
