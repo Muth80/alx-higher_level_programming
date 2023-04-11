ac08ea292c3ef466a35c6fec6cf37b64cf57bbd2
David Muindi <muindidiego@gmail.com>
2023-04-11 06:10:21 -0700

    Task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,52 +1,34 @@
 #!/usr/bin/python3
 """
-This module defines a BaseGeometry class and a Rectangle class that inherits
-from it.
+Module: 8-rectangle
+Defines a Rectangle class that inherits from BaseGeometry
 """
 
-
-class BaseGeometry:
-    """BaseGeometry class"""
-
-    def area(self):
-        """Calculate the area"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate an integer value"""
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class"""
-
+    """
+    Rectangle class that inherits from BaseGeometry
+    """
     def __init__(self, width, height):
-        """Initialize a new Rectangle instance"""
-        self.__width = 0
-        self.__height = 0
+        """
+        Initializes a new Rectangle instance with width and height
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def __str__(self):
-        """Return a string representation of the Rectangle instance"""
+        """
+        Returns a string representation of the Rectangle instance
+        """
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
     def area(self):
-        """Calculate the area of the Rectangle instance"""
+        """
+        Returns the area of the Rectangle instance
+        """
         return self.__width * self.__height
-
-    @property
-    def width(self):
-        """Return the width of the Rectangle instance"""
-        return self.__width
-
-    @property
-    def height(self):
-        """Return the height of the Rectangle instance"""
-        return self.__height
 
