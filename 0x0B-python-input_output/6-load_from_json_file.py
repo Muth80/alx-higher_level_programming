c24597967447f0cb330b143308d2e3ef75607032
David Muindi <muindidiego@gmail.com>
2023-04-12 04:51:02 -0700

    Task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,21 +1,44 @@
 #!/usr/bin/python3
+from typing import List, Dict, Any, Union
 
-# This script defines a function that loads a JSON object from a file.
-
-import json
-
-def load_from_json_file(filename):
-    """
-    Load a JSON object from a file.
+def load_from_json_file(filename: str) -> Union[List, Dict[str, Any]]:
+    """Load a JSON object from a file.
 
     Args:
         filename (str): The name of the JSON file.
 
     Returns:
-        dict: The deserialized JSON object.
-
+        Union[List, Dict[str, Any]]: The deserialized JSON object.
     """
-    with open(filename) as f:
-        data = json.load(f)
-    return data
+    import json
+    with open(filename, 'r') as f:
+        return json.load(f)
+
+
+if __name__ == '__main__':
+    filename = "my_list.json"
+    my_list = load_from_json_file(filename)
+    print(my_list)
+    print(type(my_list))
+
+    filename = "my_dict.json"
+    my_dict = load_from_json_file(filename)
+    print(my_dict)
+    print(type(my_dict))
+
+    try:
+        filename = "my_set_doesnt_exist.json"
+        my_set = load_from_json_file(filename)
+        print(my_set)
+        print(type(my_set))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        filename = "my_fake.json"
+        my_fake = load_from_json_file(filename)
+        print(my_fake)
+        print(type(my_fake))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
 
