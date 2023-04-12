79731047884194581a92aed79a96eca2d9df48bc
David Muindi <muindidiego@gmail.com>
2023-04-12 03:44:35 -0700

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""
+This module defines read_file function
+"""
 
 def read_file(filename=""):
-    """
-    Reads a text file (UTF8) and prints it to stdout.
+    """Reads a text file and prints it to stdout.
 
     Args:
         filename (str): The name of the file to read.
@@ -10,6 +12,7 @@ def read_file(filename=""):
     Returns:
         None
     """
-    with open(filename, mode='r', encoding='utf8') as file:
-        print(file.read(), end='')
+    with open(filename, mode='r', encoding='utf-8') as f:
+        print(f.read(), end="")
+
 
