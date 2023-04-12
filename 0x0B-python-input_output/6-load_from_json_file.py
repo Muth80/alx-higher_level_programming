98eec4708cf5cd7b41cf7fc3304e1cf0a576c5d4
David Muindi <muindidiego@gmail.com>
2023-04-12 04:30:57 -0700

    Task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import json
+
+def load_from_json_file(filename):
+    """
+    Load a JSON object from a file.
+
+    Args:
+        filename (str): The name of the JSON file.
+
+    Returns:
+        dict: The deserialized JSON object.
+
+    Raises:
+        FileNotFoundError: If the file does not exist.
+        JSONDecodeError: If the file contains invalid JSON.
+
+    """
+    # Open the file with 'with' statement to ensure it is properly closed when the block is exited
+    with open(filename) as f:
+        # Use the 'json.load' function to read the contents of the file and deserialize the JSON object
+        data = json.load(f)
+    # Return the deserialized JSON object (which is assumed to be a dictionary in this case)
+    return data
+
