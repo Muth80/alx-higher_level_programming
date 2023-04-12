4e7ad64e69d68169115134d530e9eb32c8e4a41e
David Muindi <muindidiego@gmail.com>
2023-04-12 07:06:59 -0700

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,34 +1,19 @@
 #!/usr/bin/python3
-"""
-Script that adds all arguments to a Python list and saves them to a file
-"""
+"""Adds all arguments to a Python list and saves them to a file."""
+
 import sys
+import json
 from os import path
 from typing import List
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-def add_item(args: List[str], filename: str) -> None:
-    """
-    Adds all arguments to a Python list and saves them to a file
-
-    Args:
-        args (List[str]): List of command line arguments
-        filename (str): Name of the file to save the list to
-    """
-    my_list = []
-    if path.exists(filename):
-        my_list = load_from_json_file(filename)
-
-    for arg in args:
-        my_list.append(arg)
-
-    save_to_json_file(my_list, filename)
-
-
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    add_item(args, "add_item.json")
+    file_path = "add_item.json"
+    my_list: List[str] = []
+    if path.exists(file_path):
+        my_list = load_from_json_file(file_path)
+
+    my_list.extend(sys.argv[1:])
+    save_to_json_file(my_list, file_path)
 
