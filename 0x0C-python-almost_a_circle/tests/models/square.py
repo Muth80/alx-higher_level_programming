c149194e7f4b39f73404177f5afee2bc63d242db
David Muindi <muindidiego@gmail.com>
2023-04-14 07:49:17 -0700

    Task 14

--- 0x0C-python-almost_a_circle/tests/models/square.py
+++ 0x0C-python-almost_a_circle/tests/models/square.py
@@ -1,43 +1,45 @@
 #!/usr/bin/python3
-""" Module Square """
+"""Define a Square class"""
 
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square class"""
+    """Represent a square"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Init method"""
+        """Initialize a square"""
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return a string representation of the square"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
 
     @property
     def size(self):
-        """ Getter method for size """
+        """Get the size of the square"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """ Setter method for size """
+        """Set the size of the square"""
         self.width = value
         self.height = value
 
-    def __str__(self):
-        """Return string"""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
-
     def update(self, *args, **kwargs):
-        """Update attributes"""
+        """Update the square"""
+        attrs = ["id", "size", "x", "y"]
         if args:
-            attrs = ["id", "size", "x", "y"]
-            for idx, val in enumerate(args):
-                setattr(self, attrs[idx], val)
-        else:
+            for i, arg in enumerate(args):
+                setattr(self, attrs[i], arg)
+        elif kwargs:
             for key, value in kwargs.items():
-                setattr(self, key, value)
+                if key in attrs:
+                    setattr(self, key, value)
 
     def to_dictionary(self):
-        """To dictionary"""
-        return {'id': self.id, 'size': self.width, 'x': self.x, 'y': self.y}
+        """Return a dictionary representation of the square"""
+        return {"id": self.id, "size": self.width, "x": self.x, "y": self.y}
 
