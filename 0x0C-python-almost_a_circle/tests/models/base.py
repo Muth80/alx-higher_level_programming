347504f4656b52000b70824cfe97bc806fac3cc6
David Muindi <muindidiego@gmail.com>
2023-04-14 08:04:56 -0700

    Task 16

--- 0x0C-python-almost_a_circle/tests/models/base.py
+++ 0x0C-python-almost_a_circle/tests/models/base.py
@@ -1,12 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines the Rectangle class, which represents a rectangle shape.
+"""
+
 import json
 
-
 class Base:
-    """A base class for other shapes"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new Base object"""
         if id is not None:
             self.id = id
         else:
@@ -15,8 +17,18 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Return the JSON string representation of list_dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        with open(filename, mode="w", encoding="utf-8") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                objs = [obj.to_dictionary() for obj in list_objs]
+                f.write(cls.to_json_string(objs))
 
