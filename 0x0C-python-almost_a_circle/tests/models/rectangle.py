dd902c69eb24b6d0f442efca491a0b861142eb5f
David Muindi <muindidiego@gmail.com>
2023-04-14 06:14:59 -0700

    Task 8

--- 0x0C-python-almost_a_circle/tests/models/rectangle.py
+++ 0x0C-python-almost_a_circle/tests/models/rectangle.py
@@ -1,89 +1,92 @@
 #!/usr/bin/python3
-"""Module for Rectangle class"""
+""" Rectangle Module """
+
 
 class Rectangle:
     """Rectangle class"""
 
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """Initializes an instance of the Rectangle class"""
+    def __init__(self, width=0, height=0, x=0, y=0, id=None):
+        """Rectangle Instantiation"""
+        self.id = id
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        self.id = id
 
     @property
     def width(self):
-        """Getter method for width"""
+        """width getter"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter method for width"""
+        """width setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("width must be > 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
-        """Getter method for height"""
+        """height getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter method for height"""
+        """height setter"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("height must be > 0")
-        self.__height = value
+        else:
+            self.__height = value
 
     @property
     def x(self):
-        """Getter method for x"""
+        """x getter"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Setter method for x"""
+        """x setter"""
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("x must be >= 0")
-        self.__x = value
+        else:
+            self.__x = value
 
     @property
     def y(self):
-        """Getter method for y"""
+        """y getter"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Setter method for y"""
+        """y setter"""
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("y must be >= 0")
-        self.__y = value
+        else:
+            self.__y = value
 
     def area(self):
-        """Returns the area of the Rectangle instance"""
-        return self.width * self.height
+        """Calculate and return the area value of the Rectangle"""
+        return self.__width * self.__height
 
     def display(self):
-        """Prints in stdout the Rectangle instance with the character '#'"""
-        for i in range(self.y):
+        """Print the Rectangle instance with the character '#'"""
+        for y in range(self.__y):
             print()
-        for i in range(self.height):
-            for j in range(self.x):
-                print(" ", end="")
-            for j in range(self.width):
-                print("#", end="")
-            print()
+        for i in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
-        """Overrides the __str__ method"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+        """Return [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
+                                                       self.__y, self.__width,
+                                                       self.__height)
 
