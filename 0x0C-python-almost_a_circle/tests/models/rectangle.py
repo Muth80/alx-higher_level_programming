fb5ce9ef8bc3b44e0a01171c41309a412cec6be9
David Muindi <muindidiego@gmail.com>
2023-04-14 06:01:55 -0700

    Task 4

--- 0x0C-python-almost_a_circle/tests/models/rectangle.py
+++ 0x0C-python-almost_a_circle/tests/models/rectangle.py
@@ -1,73 +1,110 @@
 #!/usr/bin/python3
-"""Module for Rectangle class"""
+"""
+Module for Rectangle class
+"""
 
-from models.base import Base
 
+class Rectangle:
+    """
+    Class that defines a rectangle
+    """
 
-class Rectangle(Base):
-    """Rectangle class that inherits from Base"""
-    
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Constructor method for Rectangle"""
-        super().__init__(id)
+        """
+        Initializes a new instance of the Rectangle class
+        """
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        
+        self.id = id
+
     @property
     def width(self):
-        """Getter method for width"""
+        """ Getter method for width """
         return self.__width
-    
+
     @width.setter
     def width(self, value):
-        """Setter method for width"""
+        """ Setter method for width """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("width must be > 0")
-        self.__width = value
-        
+        else:
+            self.__width = value
+
     @property
     def height(self):
-        """Getter method for height"""
+        """ Getter method for height """
         return self.__height
-    
+
     @height.setter
     def height(self, value):
-        """Setter method for height"""
+        """ Setter method for height """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("height must be > 0")
-        self.__height = value
-        
+        else:
+            self.__height = value
+
     @property
     def x(self):
-        """Getter method for x"""
+        """ Getter method for x """
         return self.__x
-    
+
     @x.setter
     def x(self, value):
-        """Setter method for x"""
+        """ Setter method for x """
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("x must be >= 0")
-        self.__x = value
-        
+        else:
+            self.__x = value
+
     @property
     def y(self):
-        """Getter method for y"""
+        """ Getter method for y """
         return self.__y
-    
+
     @y.setter
     def y(self, value):
-        """Setter method for y"""
+        """ Setter method for y """
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("y must be >= 0")
-        self.__y = value
+        else:
+            self.__y = value
+
+    def area(self):
+        """ Method that returns the area of the rectangle """
+        return self.__width * self.__height
+
+    def display(self):
+        """ Method that prints the rectangle to stdout """
+        print('\n' * self.__y, end="")
+        for i in range(self.__height):
+            print(' ' * self.__x, end="")
+            print('#' * self.__width)
+
+    def __str__(self):
+        """ Method that returns the string representation of the rectangle """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args, **kwargs):
+        """ Method that updates the attributes of the rectangle """
+        if args:
+            attrs = ["id", "width", "height", "x", "y"]
+            for i, arg in enumerate(args):
+                setattr(self, attrs[i], arg)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ Method that returns the dictionary representation of the rectangle """
+        return {"id": self.id, "width": self.__width, "height": self.__height, "x": self.__x, "y": self.__y}
 
