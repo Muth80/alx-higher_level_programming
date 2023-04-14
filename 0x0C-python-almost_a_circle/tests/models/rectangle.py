d7b56f8e5fa70d5b67b32cf750a40a8b0fa17704
David Muindi <muindidiego@gmail.com>
2023-04-14 06:39:19 -0700

    Task 9

--- 0x0C-python-almost_a_circle/tests/models/rectangle.py
+++ 0x0C-python-almost_a_circle/tests/models/rectangle.py
@@ -1,38 +1,96 @@
 #!/usr/bin/python3
 """ Module for Rectangle class """
 
-class Rectangle:
-    """ A Rectangle class """
+class Rectangle(Base):
+    """Rectangle class"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ Initializes an instance of the Rectangle class """
+        """Initializer"""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        if id is None:
-            Rectangle.count += 1
-            self.id = Rectangle.count
-        else:
-            self.id = id
+
+    @property
+    def width(self):
+        """Width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Width setter"""
+        self.__width = value if value > 0 else raise ValueError("width must be > 0")
+
+    @property
+    def height(self):
+        """Height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Height setter"""
+        self.__height = value if value > 0 else raise ValueError("height must be > 0")
+
+    @property
+    def x(self):
+        """X getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """X setter"""
+        self.__x = value if value >= 0 else raise ValueError("x must be >= 0")
+
+    @property
+    def y(self):
+        """Y getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Y setter"""
+        self.__y = value if value >= 0 else raise ValueError("y must be >= 0")
 
     def area(self):
-        """ Returns the area of the Rectangle """
+        """Area computation"""
         return self.width * self.height
 
     def display(self):
-        """ Prints the Rectangle with '#' characters """
-        for i in range(self.y):
-            print()
+        """Displays the rectangle using the '#' character"""
+        print("\n" * self.y, end="")
         for i in range(self.height):
-            print(" " * self.x + "#" * self.width)
+            print(" " * self.x, end="")
+            print("#" * self.width)
 
     def __str__(self):
-        """ Returns a string representation of the Rectangle """
+        """Returns string representation"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
 
-    def update(self, *args):
-        """ Assigns an argument to each attribute of the Rectangle """
-        attributes = ["id", "width", "height", "x", "y"]
-        for i, arg in enumerate(args):
-            setattr(self, attributes[i], arg)
+    def update(self, *args, **kwargs):
+        """Updates attributes"""
+        if len(args) > 0:
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.width = arg
+                elif i == 2:
+                    self.height = arg
+                elif i == 3:
+                    self.x = arg
+                elif i == 4:
+                    self.y = arg
+        elif len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                elif key == 'width':
+                    self.width = value
+                elif key == 'height':
+                    self.height = value
+                elif key == 'x':
+                    self.x = value
+                elif key == 'y':
+                    self.y = value
 
