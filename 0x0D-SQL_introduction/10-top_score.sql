43e5da162d0371488ca876ff17515b440819fd74
David Muindi <muindidiego@gmail.com>
2023-05-16 03:04:23 -0700

    Task 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- This script lists all records of the table second_table from the hbtn_0c_0 database
+-- The results display both the score and the name columns, ordered by score (top first)
+SELECT score, name
+FROM hbtn_0c_0.second_table
+ORDER BY score DESC;
