b3a8f293f1c039b23d9f7139be41d0f10154e147
David Muindi <muindidiego@gmail.com>
2023-05-16 02:48:20 -0700

    Task 5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- This script retrieves the full description of the table first_table from the hbtn_0c_0 database
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0'
+  AND TABLE_NAME = 'first_table';
