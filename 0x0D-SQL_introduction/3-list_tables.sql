4734a40eb52ed6214d2f3be79cd3bbe2229d56b6
David Muindi <muindidiego@gmail.com>
2023-05-16 04:17:01 -0700

    Changes

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,7 +1,2 @@
--- This script sets the database context to mysql
-USE `mysql`;
-
 -- This script lists all the tables in the current database
-SELECT table_name AS Tables
-FROM information_schema.tables
-WHERE table_schema = DATABASE();
+SHOW TABLES;
